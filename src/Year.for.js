2bc6d71f2ada1ad12bc81879e559fff3ebc6947a
Jonathan Brincat <jonathan@pix8.co.uk>
2019-01-02 16:22:24 +0000

    removed legacy reference js; split different implementations into distinct scripts prior to selection of approach

---
+++ src/Year.for.js
@@ -0,0 +1,45 @@
+import GregorianDay from './SakamotoMethod'
+//import Month from './month'
+import en from './locales/en'
+
+
+export default class Year {
+
+	constructor(_epoch) {
+
+		this.epoch = {
+			year: parseInt(_epoch.getUTCFullYear(), 10),
+			month: parseInt(1, 10),
+			date: parseInt(1, 10)
+		}
+
+		var calendarOffset = GregorianDay(this.epoch.year, this.epoch.month, this.epoch.date);
+
+		this.calendarYear = [];
+
+		let yearDayCount = 0;
+
+		Year.STATICS.LOOKUPTABLE.forEach((no_OfdaysInMonth, i, arr) => {
+
+			// calibration for presence of leap year
+			if(Array.isArray(no_OfdaysInMonth)) no_OfdaysInMonth = no_OfdaysInMonth[~~this.isLeapYear()];
+
+			let calendarMonth = new Array(no_OfdaysInMonth);
+
+			for(let j = 0, l = calendarMonth.length; j < l; j++) {
+				calendarMonth[j] = (yearDayCount + calendarOffset)%7;
+				yearDayCount++;
+			};
+
+			this.calendarYear[i] = calendarMonth;
+		});
+	}
+
+	isLeapYear() {
+		return Boolean( (!(this.epoch.year%4) && this.epoch.year%100) || !(this.epoch.year%400) );
+	};
+}
+
+Year.STATICS = {
+	LOOKUPTABLE: [31, [28, 29], 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+};
---
+++ src/Year.reduce.js
@@ -0,0 +1,40 @@
+import GregorianDay from './SakamotoMethod'
+//import Month from './month'
+import en from './locales/en'
+
+
+export default class Year {
+
+	constructor(_epoch) {
+
+		this.epoch = {
+			year: parseInt(_epoch.getUTCFullYear(), 10),
+			month: parseInt(1, 10),
+			date: parseInt(1, 10)
+		}
+
+		var calendarOffset = GregorianDay(this.epoch.year, this.epoch.month, this.epoch.date);
+
+		this.calendarYear = [ [...Array(Year.STATICS.LOOKUPTABLE[0])].map( (item, j) => ( j + calendarOffset )%7 ) ];
+
+		Year.STATICS.LOOKUPTABLE.reduce( (tally, curr, i) => {
+			//console.log(i, tally, " :: ", curr);
+
+			if(Array.isArray(curr)) curr = curr[~~this.isLeapYear()];
+
+			let calendarMonth = [...Array(curr)].map( (item, j) => ( (j+tally) + calendarOffset )%7 );
+
+			this.calendarYear[i] = calendarMonth;
+
+			return tally + curr;
+		});
+	}
+
+	isLeapYear() {
+		return Boolean( (!(this.epoch.year%4) && this.epoch.year%100) || !(this.epoch.year%400) );
+	};
+}
+
+Year.STATICS = {
+	LOOKUPTABLE: [31, [28, 29], 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+};
