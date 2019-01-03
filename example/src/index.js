7d0e3418d37aef8430f79ad07bd4d8997db7b7c6
Jonathan Brincat <jonathan@pix8.co.uk>
2019-01-03 09:35:56 +0000

    early release diluted down; odd code refinement and tidy up

--- example/src/index.js
+++ example/src/index.js
@@ -12,36 +12,46 @@ var calendar = new Calendar();
 
 input.addEventListener("change", (event) => { //OR old way .onchange = (event) => {}; and .onchange() to trigger
 
-	console.log("EPOCH :: ", event.target.value);
+	console.log("EPOCH = ", event.target.value);
+	if(!event.target.value.length) return false;
 
 	calendar.getYear(event.target.value).then(data => {
 		console.log("Year = ", data);
 		
-		document.getElementById("payload-getyear").innerHTML = JSON.stringify(data);
+		var el = document.getElementById("payload--getyear");
+		if(el !== null) el.innerHTML = JSON.stringify(data);
 	});
 
 	calendar.getMonth(event.target.value).then(data => {
 		console.log("Month = ", data);
 		
-		document.getElementById("payload-getmonth").innerHTML = JSON.stringify(data);
+		var el = document.getElementById("payload--getmonth")
+		if(el !== null) el.innerHTML = JSON.stringify(data);
 	});
 
 	calendar.getWeek(event.target.value).then(data => {
 		console.log("Week = ", data);
 		
-		document.getElementById("payload-getweek").innerHTML = JSON.stringify(data);
+		var el = document.getElementById("payload--getweek")
+		if(el !== null) el.innerHTML = JSON.stringify(data);
 	});
 
-	/*calendar.getDay(event.target.value).then(data => {
-		console.log("Week = ", data);
+	calendar.getDay(event.target.value).then(data => {
+		console.log("Day = ", data);
 		
-		document.getElementById("payload-getweek").innerHTML = JSON.stringify(data);
-	});*/
+		var el = document.getElementById("payload--getday")
+		if(el !== null) el.innerHTML = JSON.stringify(data);
+	});
+
+	document.querySelectorAll("span.query__year").forEach( el => el.innerHTML = new Date(event.target.value).getFullYear() );
+	document.querySelectorAll("span.query__month").forEach( el => el.innerHTML = calendar.locales.MONTH[ new Date(event.target.value).getMonth() ] );
+	document.querySelectorAll("span.query__date").forEach( el => el.innerHTML = new Date(event.target.value).getDate() );
+	document.querySelectorAll("span.query__day").forEach( el => el.innerHTML = calendar.locales.DAY[ new Date(event.target.value).getDay() ] );
 });
 
 
-//format = yyyy-MM-dd
+// Pre-populate input with today's date
+// format = yyyy-MM-dd
 input.value = [epoch.getUTCFullYear(), `${"0" + (epoch.getUTCMonth()+1)}`.slice(-2), `${"0" + epoch.getUTCDate()}`.slice(-2)].join("-");
-
 var event = new Event("change", {bubbles: false});
 input.dispatchEvent(event);
